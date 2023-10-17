56bd48fbd62ddb38553a793f068290af261e1d8b
louisa <82815171+v1peridae@users.noreply.github.com>
2023-10-17 03:36:57 -0700

    Create question-2.py
    https://replit.com/@viperidae/Assignment-32#main.py

---
+++ question-2.py
@@ -0,0 +1,5 @@
+day = input('Please type the day of the week in this format here : Mon , Tue , Wed , Thur, Fri, Sat, Sun >>>')
+if day == 'Sat''Sun':
+    print("Hooray – it’s the weekend!")
+else:
+    print (" “Bad luck – back to your studies.")
